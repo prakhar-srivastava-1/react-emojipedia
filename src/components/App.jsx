e61311e01e3d28935934e813f75f6cc476fc2d5b
Prakhar Srivastava <50711734+prakhar-srivastava-1@users.noreply.github.com>
2022-02-27 08:09:29 +0530

    use arrow function for brevity Remove the createEntry function and instead use Arrow Function to display all the entries,
    {Entries.map((entry) => (
         <Entry key={entry.id} emoji={entry.emoji} name={entry.name} meaning={entry.meaning} />
           ))};

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,6 @@ import React from "react";
 import Entry from "./Entry.jsx";
 import Entries from "../emojipedia.js";
 
-function createEntry(entry) {
-  return (
-    <Entry
-      key={entry.id}
-      emoji={entry.emoji}
-      name={entry.name}
-      meaning={entry.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
@@ -21,7 +10,16 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">{Entries.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {Entries.map((entry) => (
+          <Entry
+            key={entry.id}
+            emoji={entry.emoji}
+            name={entry.name}
+            meaning={entry.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
